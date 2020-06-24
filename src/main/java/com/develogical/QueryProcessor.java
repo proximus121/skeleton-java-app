8f1b38280a64e4c4ec38ebbb10222501150da6fd
proximus121 <mirce0ne@gmail.com>
2020-06-24 13:09:44 +0300

    sqrt

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,5 +1,11 @@
 package com.develogical;
 
+import jdk.internal.joptsimple.internal.Strings;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class QueryProcessor {
 
     public String process(String query) {
@@ -9,6 +15,19 @@ public class QueryProcessor {
 
         if(query.toLowerCase().contains("theresa may")){
             return "2019";
+        }
+
+        if (query.contains("square")) {
+            String[] split = query.substring(query.indexOf(":")).split(",");
+            return Arrays.asList(split).stream().map(s -> {
+                double var = Math.sqrt(Double.parseDouble(s));
+                if (var == Math.floor(var)) {
+                    double var1 = Math.sqrt(var);
+                    if (var1 == Math.floor(var1)) ;
+                }
+                return s;
+
+            }).collect(Collectors.toList()).toString();
         }
         return "";
     }
