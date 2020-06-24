997d773afd9e1fbec6d7e6bb42a72b502ac3577b
proximus121 <mirce0ne@gmail.com>
2020-06-24 13:13:38 +0300

    sqrt2

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,8 +1,5 @@
 package com.develogical;
 
-import jdk.internal.joptsimple.internal.Strings;
-
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -13,20 +10,19 @@ public class QueryProcessor {
             return "William Shakespeare";
         }
 
-        if(query.toLowerCase().contains("theresa may")){
+        if (query.toLowerCase().contains("theresa may")) {
             return "2019";
         }
 
         if (query.contains("square")) {
             String[] split = query.substring(query.indexOf(":")).split(",");
-            return Arrays.asList(split).stream().map(s -> {
+            return Arrays.stream(split).filter(s -> {
                 double var = Math.sqrt(Double.parseDouble(s));
                 if (var == Math.floor(var)) {
                     double var1 = Math.sqrt(var);
-                    if (var1 == Math.floor(var1)) ;
+                    return var1 == Math.floor(var1);
                 }
-                return s;
-
+                return false;
             }).collect(Collectors.toList()).toString();
         }
         return "";
